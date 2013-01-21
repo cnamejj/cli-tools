7988ca151c9dbecf96a6b799ff35c09f84c94fd4
Jim Jones <cnamejj@gmail.com>
2013-01-21 02:33:59 -0800

    Add a new option type "OP_TYPE_LAST" that grabs the rest of the command line

--- parse_opt.h
+++ parse_opt.h
@@ -19,6 +19,7 @@
 #define OP_TYPE_INT   3
 #define OP_TYPE_VOID  4
 #define OP_TYPE_FLOAT 5
+#define OP_TYPE_LAST  6
 
 #define OP_FL_BLANK   0x00
 #define OP_FL_FOUND   0x01
@@ -42,11 +43,18 @@ struct word_chain {
    struct word_chain *next;
 };
 
+struct word_list {
+   int count;
+   char **words;
+};
+
 /* --- */
 
 struct word_chain *parse_command_options( int *rc, struct option_set *plist, int nopt, int narg, char **opts);
 
 struct option_set *get_matching_option( int flag_num, struct option_set *opset, int nflags);
+
+void print_parse_summary( struct word_chain *, struct option_set *, int);
 
 /* --- */
 
